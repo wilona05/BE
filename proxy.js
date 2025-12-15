81ae96cc3aa2ef7a2b0a146d3ecf266580efd044
wilona05 <wilona.muliadi@gmail.com>
2025-12-15 17:24:30 +0700

    add proxy

---
+++ proxy.js
@@ -0,0 +1,38 @@
+import https from "node:https";
+import http from "node:http";
+import fs from "node:fs";
+import path from "node:path";
+
+// https certificate
+const privateKeyPath = path.resolve(import.meta.dirname, "private-key.pem");
+const certificatePath = path.resolve(import.meta.dirname, "certificate.pem");
+const privateKey = fs.readFileSync(privateKeyPath);
+const certificate = fs.readFileSync(certificatePath);
+const proxy = new https.Server({
+   key: privateKey,
+   cert: certificate,
+})
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+proxy.on("request", (request, response) => {
+    const method = request.method;
+    const path = request.url;
+
+    const server = http.request({
+        host: hostname,
+        port: port,
+        method: method,
+        path: path,
+        headers: request.headers,
+    });
+    request.pipe(server);
+
+    server.on("response", (serverResponse) => {
+        serverResponse.pipe(response);
+    });
+});
+
+proxy.listen(8080);
+console.log('The proxy is running! Check it on https://localhost:8080/');
